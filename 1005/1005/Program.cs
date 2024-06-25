6d5e2d4e58daf1cb46404fd6e16a29e42b045a0b
Marcos Costa <marcoscosta@air-de-marcos.lan>
2024-06-25 13:23:57 +0100

    Complete app

---
+++ 1005/1005/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insira a base do retangulo = ");
+            double Base = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a altura do retangulo = ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double Area = Base * altura;
+            double Perimetro = 2 * (Base + altura);
+            double Diagonal = Math.Sqrt(Math.Pow(Base, 2.0) + Math.Pow(altura, 2.0));
+
+            Console.WriteLine("AREA = " + Area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + Perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + Diagonal.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
